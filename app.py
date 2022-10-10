3ee00d11d38582e37052a694e912e41cfc207ac9
Thomas Waggoner <thomwagg@iu.edu>
2022-10-10 09:58:27 -0400

    install bootstrap

--- app.py
+++ app.py
@@ -4,19 +4,23 @@ app = Flask(__name__)
 
 with open('dinosaurs.csv', 'r') as csvfile:
     data = csv.DictReader(csvfile)
-    for row in data:
-        dinosaurs = {row['slug']: {'name':row['name'],'description':row['description'],'image':row['image'],'image-credit':row['source-credit']}}
+    dinosaurs = {row['slug']: {'name':row['name'],'description':row['description'],'image':row['image'],'image-credit':row['source-credit']}for row in data}
     
     print(dinosaurs)
 
 @app.route('/')
-def index():
-    return render_template('index.html', name="Thomas", dinosaurs = dinosaurs)
+@app.route('/dino')
+@app.route('/dino/<dino>')
+def index(dino=None):
+    return render_template('index.html', dinosaurs=dinosaurs)
+    #make sure dino exists
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# @app.route('/dino/<dino>')
-# def index(dino=None):
-#     return render_template('index.html', dinosaurs=dinosaurs)
+@app.route('/FAQ')
+def FAQ():
+    return render_template('FAQ.html')
+
